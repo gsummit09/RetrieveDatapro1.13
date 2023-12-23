6fcef8a4e3e2114c9753ffe912882b3d0ee99586
Sumit Nayak <96038589+gsummit09@users.noreply.github.com>
2023-12-23 13:41:06 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RetrieveDataStudent1._13
+{
+    internal class Program
+    {
+        public static void Fun1()
+        {
+            string pathFile = @"D:\\Console Asp\RetrieveDataStudent1.13\\StudentData.txt";
+            if (File.Exists(pathFile))
+            {
+                StreamReader sr = new StreamReader(pathFile);
+                string tillEnd = sr.ReadToEnd();
+                Console.WriteLine(tillEnd);
+                sr.Close();
+            }
+            else
+            {
+                Console.WriteLine("File Not Found\nPlease Create StudentData.txt File\n");
+                StreamWriter writer = new StreamWriter(pathFile);
+                writer.Close();
+            }
+        }
+        static void Main(string[] args)
+        {
+            Fun1();
+        }
+    }
+}
